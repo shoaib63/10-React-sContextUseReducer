2c0550cc01e7e6cffeb0abbb20b9950a559707d2
shoaib63 <mirakishoaib@gmail.com>
2024-08-08 12:19:24 +0330

    not Completed

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { useState } from 'react';
 import Header from './components/Header.jsx';
 import Shop from './components/Shop.jsx';
 import { DUMMY_PRODUCTS } from './dummy-products.js';
+import { CartContext } from './shop/shopping-cart-context.jsx';
 
 function App() {
   const [shoppingCart, setShoppingCart] = useState({
@@ -66,13 +67,13 @@ function App() {
   }
 
   return (
-    <>
+    <CartContext.Provider value={{items: []}}>
       <Header
         cart={shoppingCart}
         onUpdateCartItemQuantity={handleUpdateCartItemQuantity}
       />
       <Shop onAddItemToCart={handleAddItemToCart} />
-    </>
+    </CartContext.Provider>
   );
 }
 
